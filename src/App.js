b03f124608240746b07a6d2cfa0c1f4387454097
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-11-18 12:53:41 +0100

    Getting user input and populating items array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import ThemeContext from './contexts/theme-context'
+import ThemeContext from "./contexts/theme-context";
 import "./App.css";
 
+const itemsToBuy = [];
+
 function App() {
-const themeCtx = useContext(ThemeContext);
+  const themeCtx = useContext(ThemeContext);
+  const [enteredItems, setEnteredItems] = useState(itemsToBuy);
+
+  const addingNewItemHandler = (dataObj) => {
+    setEnteredItems((prevState) => {
+      console.log(enteredItems);
+      return [dataObj, ...prevState];
+    });
+  };
 
   return (
-    <div id="app" className={themeCtx.lightMode ? 'lightMode' : ''}>
+    <div id="app" className={themeCtx.lightMode ? "lightMode" : ""}>
       <Header />
       <main>
-      <Form />
+        <Form onAddingNewItem={addingNewItemHandler} />
       </main>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Form.module.css";
 
-const Form = () => {
-    return (
-  <form type="submit" className={classes.form__controls}>
-    <div className={classes.form__control}>
-    <label htmlFor="title">Title</label>
-    <input id="title" name="title" type="text"></input>
-    </div>
-    <div className={classes.form__control}>
-    <label htmlFor="amount">Amount</label>
-    <input id="amount" name="amount" type="number"></input>
-    </div>
-    <button type="submit">Add to list</button>
-  </form>
-    );
+const Form = (props) => {
+  const [newShopItem, setNewShopItem] = useState({itemTitle: '', itemAmount:''});
+ 
+
+  const titleChangeHandler = (e) => {
+    setNewShopItem((prevState) => {
+      return {
+        ...prevState,
+        itemTitle: e.target.value,
+      };
+    });
+  };
+
+  const amountChangeHandler = (e) => {
+    setNewShopItem((prevState) => {
+      return {
+        ...prevState,
+        itemAmount: e.target.value,
+      };
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const itemData = {
+      title: newShopItem.itemTitle,
+      amount: newShopItem.itemAmount,
+      id: Math.random().toString()
+    }
+    console.log(itemData);
+    props.onAddingNewItem(itemData)
+    setNewShopItem({itemTitle: '', itemAmount: ''});
+  };
+
+  return (
+    <form
+      type="submit"
+      className={classes.form__controls}
+      onSubmit={onSubmitHandler}
+    >
+      <div className={classes.form__control}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={titleChangeHandler}
+          value={newShopItem.itemTitle}
+        ></input>
+      </div>
+      <div className={classes.form__control}>
+        <label htmlFor="amount">Amount</label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          onChange={amountChangeHandler}
+          value={newShopItem.itemAmount}
+        ></input>
+      </div>
+      <div className={classes.form__control}>
+        <button type="submit">Add to list</button>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
