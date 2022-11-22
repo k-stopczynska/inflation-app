248a180d68f9a7c63ce54901025748c330a70712
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-11-22 19:27:28 +0100

    fixing context for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '../UI/Button';
-import classes from './Modal.module.css';
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import ThemeContext from "../contexts/theme-context";
+import Button from "../UI/Button";
+import classes from "./Modal.module.css";
 
 const Modal = (props) => {
+  const themeCtx = useContext(ThemeContext);
 
-const closeModal = () => {
-props.open(false)
-}
+  const closeModal = () => {
+    props.open(false);
+  };
 
-return ReactDOM.createPortal(
-    <React.Fragment>
-        <div className={classes.overlay}/>
-            <div className={classes.modal}>{props.children}
-            <Button type="button" onClick={closeModal}>Close</Button>
-        </div>
-    </React.Fragment>,
-    document.getElementById('modal')
-)
-}
+  return ReactDOM.createPortal(
+    <div id="modal" className={themeCtx.lightMode ? "lightMode" : ""}>
+      <div className={classes.overlay} />
+      <div className={classes.modal}>
+        {props.children}
+        <Button type="button" onClick={closeModal}>
+          Close
+        </Button>
+      </div>
+    </div>,
+    document.getElementById("modal")
+  );
+};
 
 export default Modal;
